ab929541da3ed0f5c0b6fb7e3e6620d1a78d3d9a
FinocchiaroGR <A01704007@itesm.mx>
2021-05-24 18:02:19 -0500

    droplet mysql

--- Web App/util/database.js
+++ Web App/util/database.js
@@ -1,11 +1,15 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    user : 'doadmin',
-    password : 'lnlosq5d7ulifaug',
-    host : 'gigisplayhousebd-do-user-9229702-0.b.db.ondigitalocean.com',
-    port : '25060',
-    database : 'gigisplayhousedb'
+    // user : 'doadmin',
+    // password : 'lnlosq5d7ulifaug',
+    user : 'root',
+    password: 'G21248055',
+    host : '167.99.233.143',
+    // host : 'gigisplayhousebd-do-user-9229702-0.b.db.ondigitalocean.com',
+    // port : '25060',
+    // database : 'gigisplayhousedb'
+    database : 'GigisT'
 });
 
 module.exports = pool.promise();
