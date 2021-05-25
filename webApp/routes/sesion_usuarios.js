a8d1b6a5b06fe2273d5984753098c90b70c75328
FinocchiaroGR <A01704007@itesm.mx>
2021-05-24 22:41:55 -0500

    probando

--- webApp/routes/sesion_usuarios.js
+++ webApp/routes/sesion_usuarios.js
@@ -6,7 +6,7 @@ const usuariosController = require('../controllers/sesion_usuarios_controller');
 const isAuth = require('../util/is-auth.js');
 
 
-router.get('/login', usuariosController.getlogin);
+router.get('/login', isAuth, usuariosController.getlogin);
 router.post('/login', usuariosController.postlogin);
 router.get('/logout',usuariosController.logout);
 router.get('/password', isAuth,usuariosController.cambiarContraseña);
