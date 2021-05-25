5031668312c0bdc5f60cb037b90cf3145eba75cc
FinocchiaroGR <A01704007@itesm.mx>
2021-05-24 22:49:36 -0500

    probando3

--- webApp/routes/sesion_usuarios.js
+++ webApp/routes/sesion_usuarios.js
@@ -4,10 +4,12 @@ const router = express.Router();
 
 const usuariosController = require('../controllers/sesion_usuarios_controller');
 const isAuth = require('../util/is-auth.js');
+const isAuthAlready = require('../util/is-auth-already.js');
 
 
+router.get('/login', isAuthAlready, usuariosController.getlogin);
 router.get('/login', usuariosController.getlogin);
-router.post('/login', isAuth, usuariosController.postlogin);
+router.post('/login', usuariosController.postlogin);
 router.get('/logout',usuariosController.logout);
 router.get('/password', isAuth,usuariosController.cambiarContraseña);
 router.post('/cambiar-pass',isAuth, usuariosController.postCambiarContraseña);
