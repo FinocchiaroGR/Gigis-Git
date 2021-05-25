feff4f29fec3a758d7b972d1dab527156e52c17f
FinocchiaroGR <A01704007@itesm.mx>
2021-05-24 21:08:39 -0500

    typo

--- webApp/util/database.js
+++ webApp/util/database.js
@@ -1,10 +1,15 @@
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost', 
-    user: 'root',
-    database: 'gigisplayhousebd_v6',
-    password: ''
+    // user : 'doadmin',
+    // password : 'lnlosq5d7ulifaug',
+    user : 'Test1',
+    password: 'Test@password321',
+    host : 'localhost',
+    // host : 'gigisplayhousebd-do-user-9229702-0.b.db.ondigitalocean.com',
+    // port : '25060',
+    // database : 'gigisplayhousedb'
+    database : 'GigisT'
 });
 
 module.exports = pool.promise();
