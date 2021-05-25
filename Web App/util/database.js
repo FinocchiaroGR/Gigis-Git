05c5a615b9cf0b4b7696d8f4ff37dfa2986d4c4b
FinocchiaroGR <A01704007@itesm.mx>
2021-05-24 19:34:09 -0500

    averrr

--- Web App/util/database.js
+++ Web App/util/database.js
@@ -3,13 +3,13 @@ const mysql = require('mysql2');
 const pool = mysql.createPool({
     // user : 'doadmin',
     // password : 'lnlosq5d7ulifaug',
-    user : 'root',
-    password: 'password',
+    user : 'Test1',
+    password: 'Test@password321',
     host : 'localhost',
     // host : 'gigisplayhousebd-do-user-9229702-0.b.db.ondigitalocean.com',
     // port : '25060',
     // database : 'gigisplayhousedb'
-    database : 'mysql'
+    database : 'GigisT'
 });
 
 module.exports = pool.promise();
