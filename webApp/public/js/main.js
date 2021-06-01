086119b34906fd62e4361468986251407e497db7
FinocchiaroGR <78937042+FinocchiaroGR@users.noreply.github.com>
2021-05-31 23:24:44 -0500

    Delete webApp/public directory

--- webApp/public/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems);
-  });
-
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.tooltipped');
-  var instances = M.Tooltip.init(elems);
-});
