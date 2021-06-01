1b8abe292fe6757447e8ba8b112e6438c5312a96
FinocchiaroGR <A01704007@itesm.mx>
2021-06-01 00:47:45 -0500

    UTIL_BUENO

--- webApp/util/database.js
+++ webApp/util/database.js
@@ -1,27 +1,15 @@
-// const mysql = require('mysql2');
-// const password = require ('./passwordBD');
-
-// const pool = mysql.createPool({
-//     user : 'doadmin',
-//     password : 'lnlosq5d7ulifaug',
-//     // user : 'Test1',
-//     // password: 'Test@password321',
-//     // host : 'localhost',
-//     host : 'gigisplayhousebd-do-user-9229702-0.b.db.ondigitalocean.com',
-//     port : '25060',
-//     database : 'gigisplayhousedb'
-//     // database : 'GigisT'
-// });
-
-// module.exports = pool.promise();
-
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
-    host: 'localhost', 
-    user: 'root',
-    database: 'gigisplayhousebd_v6',
-    password: ''
+    // user : 'doadmin',
+    // password : 'lnlosq5d7ulifaug',
+    user : 'Test1',
+    password: 'Test@password321',
+    host : 'localhost',
+    // host : 'gigisplayhousebd-do-user-9229702-0.b.db.ondigitalocean.com',
+    // port : '25060',
+    // database : 'gigisplayhousedb'
+    database : 'GigisT'
 });
 
 module.exports = pool.promise();
